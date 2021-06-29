b91c16141954df1cdbafd0466c7da3243bbd5d6c
Sanzhar Bablanov <sanmonkey92@gmail.com>
2021-06-29 14:53:39 +0600

    authorization process modified

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-const url = "https://api.yelp.com/v3/businesses/search?location=paris";
+const url =
+  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=paris&term=sights&sort_by=rating&limit=12";
 
 const apiKey =
   "P2XEzD7Hvx_Pmb_zWlonS4RjMU0amcDGn0Gb26McJAZGOwPIZ4NLoIXmkJ9X4eP8R6n9tkGYPJ8AYs7ybkKoWe1f3wVGWdqsQWBs3Z_X86OE8J1llnQk8pugjMLZYHYx";
 
 const headers = {
-  Authorization: `${apiKey}`,
+  Authorization: `Bearer ${apiKey}`,
   "Content-Type": "application/json",
 };
-
-const userAgent =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
 
 const options = {
   headers: headers,
@@ -17,10 +15,7 @@ const options = {
 
 async function getData() {
   try {
-    const response = await fetch(
-      "https://api.yelp.com/v3/businesses/search?location=paris",
-      options
-    );
+    const response = await fetch(url, options);
     if (response.ok) {
       const jsonResponse = await response.json();
       console.log(jsonResponse);
